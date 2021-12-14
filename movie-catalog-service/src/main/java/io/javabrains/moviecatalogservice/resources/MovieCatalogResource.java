1bfd64e928162164f1676307ab79747d71a4f2ae
UrszulaP <urszulapoliszuk@gmail.com>
2021-12-14 16:37:17 +0100

    add WebClient way

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,6 +20,9 @@ public class MovieCatalogResource {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
@@ -32,9 +36,16 @@ public class MovieCatalogResource {
         // 3 - bind and return data
         return ratings.stream().map(rating -> {
             // 2 - get rated movies details
-            Movie movie = restTemplate.getForObject(
-                    "http://localhost:8082/movies/" + rating.getMovieId(), Movie.class
-            );
+            // 2a - using RestTemplate
+             Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            // 2b - using WebClient
+            // Movie movie = webClientBuilder.build()
+            //         .get()
+            //         .uri("http://localhost:8082/movies/" + rating.getMovieId())
+            //         .retrieve()
+            //         .bodyToMono(Movie.class) // Mono - kind of a "Promise", we get an async object
+            //         .block();
+
             return new CatalogItem(movie.getName(), "Desc", rating.getRating());
         })
                 .collect(Collectors.toList());
