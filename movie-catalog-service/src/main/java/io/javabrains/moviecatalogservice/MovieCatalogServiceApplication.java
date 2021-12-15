601e2da7760127e457a67cd509171fb926fdcab4
UrszulaP <urszulapoliszuk@gmail.com>
2021-12-15 19:28:04 +0100

    set request timeout - increasing fault tolerance for a slow service

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -19,7 +20,9 @@ public class MovieCatalogServiceApplication {
 	@Bean
 	@LoadBalanced // to use Eureka service discovery; provides load balancer to handle multiple service instances running
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectionRequestTimeout(3000); // 3sec; if request exceeds the time, an error is thrown and thread is ended
+		return new RestTemplate(clientHttpRequestFactory);
 	}
 
 	@Bean
