52caecc576bdc5e33a080a4861fed70df61fc93b
UrszulaP <urszulapoliszuk@gmail.com>
2021-12-19 13:59:58 +0100

    refactor - extract methods

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -29,22 +29,32 @@ public class MovieCatalogResource {
     @HystrixCommand(fallbackMethod = "getFallbackCatalog") // tells Hystrix what to break; sets fallback mechanism
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-        // 1 - get all movies that were rated by the user
+        UserRating ratings = getUserRating(userId);
+        // 3 - bind and return data
+        return ratings.getUserRating().stream()
+                .map(rating -> getCatalogItem(rating))
+                .collect(Collectors.toList());
+    }
+
+    // 1 - get all movies that were rated by the user
+    private UserRating getUserRating(String userId) {
         UserRating ratings = restTemplate.getForObject(
                 "http://ratings-data-service/ratingsdata/users/" + userId, // not a real url - protocol://<service name in Eureka service discovery>/<endpoint>
                 UserRating.class // If response type is a list, we have to set type to sth like ParametrizedType<List<Rating>> - can't use List<Rating>
         );
-
-        // 3 - bind and return data
-        return ratings.getUserRating().stream().map(rating -> {
-            // 2 - get rated movies details
-            Movie movie = restTemplate.getForObject(
-                    "http://movie-info-service/movies/" + rating.getMovieId(),
-                    Movie.class
-            ); // not a real url - protocol://<service name in Eureka service discovery>/<endpoint>
-            return new CatalogItem(movie.getName(), "Desc", rating.getRating());
-        }).collect(Collectors.toList());
+        return ratings;
     }
+
+    // 2 - get rated movies details
+    private CatalogItem getCatalogItem(io.javabrains.moviecatalogservice.models.Rating rating) {
+        Movie movie = restTemplate.getForObject(
+                "http://movie-info-service/movies/" + rating.getMovieId(),
+                Movie.class
+        ); // not a real url - protocol://<service name in Eureka service discovery>/<endpoint>
+        return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+    }
+
+
 
     // fallback response should be simple, hardcoded, at most taken from the cache
     // we shouldn't do any other call in fallback, because it can also fail and we would have to handle that too
